6a9b192612aaa16afac1dd3759f3bb169c317ef5
Devil Queen <rominafp>
2022-03-11 14:25:17 +0330

    tamrin1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿for(int i= 0; i<4; i++ )
+
+{
+    for (int x = 4; x>(i+1) ; x--)
+    {
+        System.Console.Write(" ");
+    }
+   for (int j = 0; j <=(i*2) ; j++)
+   {
+      System.Console.Write("*"); 
+   }
+   System.Console.WriteLine("");
+}
+
